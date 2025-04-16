0924ad8f3a863e79a17a4123f1342d72d14aa153
Alex Stephen <astephen2@gmail.com>
2025-04-16 16:54:14 -0700

    prettier

--- src/schema.ts
+++ src/schema.ts
@@ -1,27 +1,29 @@
 import { z } from "zod";
-import {Schema} from './common/api/types';
+import { Schema } from "./common/api/types";
 
 export function resourceToZodSchema(schema: Schema): z.ZodRawShape {
   switch (schema.type) {
-    case 'boolean':
+    case "boolean":
       return { type: z.boolean() };
-    case 'number':
+    case "number":
       return { type: z.number() };
-    case 'string':
+    case "string":
       return { type: z.string() };
-    case 'object':
-      return Object.entries(schema.properties || {}).reduce((acc: Record<string, z.ZodTypeAny>, [key, value]) => {
-        acc[key] = resourceToZodSchema(value).type;
-        return acc;
-      }, {});
-    case 'array':
-        if(!schema.items) {
-            throw "array type with no items set"
-        }
-        return { type: resourceToZodSchema(schema.items).type.array() };
+    case "object":
+      return Object.entries(schema.properties || {}).reduce(
+        (acc: Record<string, z.ZodTypeAny>, [key, value]) => {
+          acc[key] = resourceToZodSchema(value).type;
+          return acc;
+        },
+        {}
+      );
+    case "array":
+      if (!schema.items) {
+        throw "array type with no items set";
+      }
+      return { type: resourceToZodSchema(schema.items).type.array() };
 
     default:
-        throw "could not find item type " + schema.type
+      throw "could not find item type " + schema.type;
   }
 }
-
--- src/server.ts
+++ src/server.ts
@@ -1,33 +1,41 @@
-import { McpServer, ResourceTemplate } from "@modelcontextprotocol/sdk/server/mcp.js";
+import {
+  McpServer,
+  ResourceTemplate,
+} from "@modelcontextprotocol/sdk/server/mcp.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
 import { fetchOpenAPI } from "./common/openapi/openapi";
 import { APIClient } from "./common/api/api";
 import { OpenAPI as OpenAPIType } from "./common/api/types";
 import { resourceToZodSchema } from "./schema";
 
-const openapiUrl = 'https://raw.githubusercontent.com/Roblox/creator-docs/refs/heads/main/content/en-us/reference/cloud/cloud.docs.json'
-const prefix = '/cloud/v2'
+const openapiUrl =
+  "https://raw.githubusercontent.com/Roblox/creator-docs/refs/heads/main/content/en-us/reference/cloud/cloud.docs.json";
+const prefix = "/cloud/v2";
 
 async function main() {
   const openapi = await fetchOpenAPI(openapiUrl);
 
-  const a = await APIClient.fromOpenAPI(openapi as OpenAPIType, "", prefix)
+  const a = await APIClient.fromOpenAPI(openapi as OpenAPIType, "", prefix);
 
   // Create an MCP server
   const server = new McpServer({
     name: "Demo",
-    version: "1.0.0"
+    version: "1.0.0",
   });
 
   // Create sample tool for MCP.
   const resources = a.resources();
   for (const [resourceName, resource] of Object.entries(resources)) {
-    server.tool(resourceName, resourceToZodSchema(resource.schema), async (args, extra) => {
-      console.log(args);
-      return {
-        content: [{ type: "text", text: "Success" }]
-      };
-    });
+    server.tool(
+      resourceName,
+      resourceToZodSchema(resource.schema),
+      async (args, extra) => {
+        console.log(args);
+        return {
+          content: [{ type: "text", text: "Success" }],
+        };
+      }
+    );
   }
 
   const transport = new StdioServerTransport();
